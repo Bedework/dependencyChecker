e55d8a7df5f6cd21983411618e0545e3e233822e
mike <mikeadouglass@gmail.com>
2025-01-10 22:43:16 -0500

    First commit

---
+++ src/main/java/org/bedework/dependencyChecker/DependencyChecker.java
@@ -0,0 +1,13 @@
+/* ********************************************************************
+    Appropriate copyright notice
+*/
+package org.bedework.dependencyChecker;
+
+/**
+ * User: mike Date: 1/10/25 Time: 22:40
+ */
+public class DependencyChecker {
+  public static void main(final String[] args) {
+
+  }
+}
